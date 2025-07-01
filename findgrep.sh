798bd54a06274900cf16017b0254f97c044293b7
itc-s25013 <s25013@std.it-college.ac.jp>
2025-07-01 10:58:33 +0900

    コメントを追加

--- findgrep.sh
+++ findgrep.sh
@@ -4,10 +4,14 @@ pattern=$1
 directory=$2
 name=$3
 
+# 第2引数(起点ディレクトリ)が空文字列ならば、
+# デフォルト値として . (カレントディレクトリ)を設定
 if [ -z "$directory" ]; then
         directory='.'
 fi
 
+# 第3引数(検索ファイルパターン)が空文字列ならば、
+# デフォルト値として'*'を設定
 if [ -z "$name" ]; then
   name='*'
 fi
